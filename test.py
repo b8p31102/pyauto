4fb4d7141c5ed0909973ab32534768db21c31c73
b8p31102 <64205746+b8p31102@users.noreply.github.com>
2021-09-07 07:32:34 +0900

    あああ

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ from kivy.resources import resource_add_path
 
 #C:\windows\Fonts
 
-resource_add_path('c:/Windows/Fonts')  # 追加分
-LabelBase.register(DEFAULT_FONT, 'msgothic.ttc')  # 追加分
+resource_add_path('C:\Windows\Fonts')  # 追加分
+LabelBase.register(DEFAULT_FONT, 'HGRMB.ttc')  # 追加分
 
 class TestApp(App):
     pass
